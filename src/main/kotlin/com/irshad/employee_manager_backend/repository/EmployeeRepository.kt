9946d64fdd5c87968cb04e1da64eb7a9dcd49a11
Irshad <alamirshad1366@gmail.com>
2026-07-03 16:51:48 +0530

    feat: implement employee search functionality

--- src/main/kotlin/com/irshad/employee_manager_backend/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/irshad/employee_manager_backend/repository/EmployeeRepository.kt
@@ -4,4 +4,11 @@ import com.irshad.employee_manager_backend.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface EmployeeRepository : JpaRepository<Employee, Long> {
+
+    fun findByNameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrDepartmentContainingIgnoreCaseOrContactNumberContainingIgnoreCase(
+        name: String,
+        email: String,
+        department: String,
+        contactNumber: String
+    ): List<Employee>
 }
